9e95bd8e140d554ce159afa90f181de1d074036b
HimanshiJain <himshijain@gmail.com>
2016-07-16 11:49:50 +0530

    comment added

--- RemoteAccessPC/app/src/main/java/com/selfstudy/himanshijain/remoteaccesspc/Constants.java
+++ RemoteAccessPC/app/src/main/java/com/selfstudy/himanshijain/remoteaccesspc/Constants.java
@@ -6,7 +6,7 @@ package com.selfstudy.himanshijain.remoteaccesspc;
 public class Constants {
 
     public static String MOUSE_LEFT_CLICK="left_click";
-    public static String SERVER_IP="*";
+    public static String SERVER_IP="*";   // add the IP address assigned to the laptop using WI-FI
     public static String PLAY="play";
     public static String NEXT="next";
     public static String EXIT="exit";
