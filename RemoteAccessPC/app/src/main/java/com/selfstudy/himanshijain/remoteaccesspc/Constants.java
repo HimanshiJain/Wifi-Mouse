9de0e7abe358c44f85b66947d2cdea5afc5339a3
HimanshiJain <himshijain@gmail.com>
2016-07-16 11:41:07 +0530

    java server and android client

---
+++ RemoteAccessPC/app/src/main/java/com/selfstudy/himanshijain/remoteaccesspc/Constants.java
@@ -0,0 +1,15 @@
+package com.selfstudy.himanshijain.remoteaccesspc;
+
+/**
+ * Created by Himanshi Jain on 4/3/2016.
+ */
+public class Constants {
+
+    public static String MOUSE_LEFT_CLICK="left_click";
+    public static String SERVER_IP="192.168.1.6";
+    public static String PLAY="play";
+    public static String NEXT="next";
+    public static String EXIT="exit";
+    public static String PREVIOUS="previous";
+    public static int SERVER_PORT=8998;
+}
